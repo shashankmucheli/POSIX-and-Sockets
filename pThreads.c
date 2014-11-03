bd52178ecf761b01160c82444d6e609ef1149228
shashankmucheli <shashank.venku@gmail.com>
2014-11-03 14:45:52 -0500

    checking rsa

--- pThreads.c
+++ pThreads.c
@@ -23,7 +23,6 @@ int main(void)
    int thread_args[NUM_THREADS];
    int rc, i;
  
-   // create all threads one by one
    for (i=0; i<NUM_THREADS; ++i) {
       thread_args[i] = i;
       printf("In main: creating thread %d\n", i);
